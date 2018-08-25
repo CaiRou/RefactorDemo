f32c42ce830bfa45641fe0dd66d07e8d147368a3
cckevincyh <835287880@qq.com>
2018-08-25 20:16:21 +0800

    add test method : should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -47,4 +47,18 @@ public class CustomerTest {
                 "You earned 1 frequent renter points", statement);
     }
 
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day() {
+        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Rental oneDayRental = new Rental(newReleaseMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Kevin\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
 }
