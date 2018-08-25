a2209ed41b3800028cd446e1291ece48bca6b338
cckevincyh <835287880@qq.com>
2018-08-25 20:26:49 +0800

    在customer.java中抽取出租赁计费规则，抽取成getCharge方法，传入Rental对象

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -25,26 +25,11 @@ public class Customer {
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while(rentals.hasMoreElements()){
-            double thisAmount =0;
+            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            switch (each.getMovie().getPriceCode()){
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if(each.getDayRented() > 2){
-                        thisAmount+=(each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount+=each.getDayRented()*3;
-                    break;
-                    case Movie.CHILDRENS:
-                        thisAmount+=1.5;
-                        if(each.getDayRented() > 3){
-                            thisAmount += (each.getDayRented() -3)*1.5;
-                        }
-                        break;
-            }
+            //抽取租赁计费的规则
+            thisAmount = getCharge(each);
 
             //add frequent renter points
             frequentRenterPoints ++;
@@ -62,5 +47,33 @@ public class Customer {
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+
+    /**
+     * 租赁计费
+     * @param rental
+     * @return
+     */
+    private double getCharge(Rental rental){
+        double thisAmount = 0;
+        switch (rental.getMovie().getPriceCode()){
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if(rental.getDayRented() > 2){
+                    thisAmount += (rental.getDayRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDayRented()*3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if(rental.getDayRented() > 3){
+                    thisAmount += (rental.getDayRented() -3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
